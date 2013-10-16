6dc7ce0687a22f0a7275b6aa13db2a8255a8fec4
jamtrousers <jamtrousers@gmail.com>
2013-10-16 21:43:43 +0100

    change factory to return function not connection

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ var partials = function(req,res) {
 
 app.get('/partials/*',partials);
 
-DB = require("mongojs").connect("eyeball", ["urls"])["urls"];
+DB = require("mongojs").connect("mongodb://eyeball:eyeball@ds047958.mongolab.com:47958/eyeball", ["urls"])["urls"];
 
 var server = http.createServer(app);
 server.listen(app.get('port'), function(){
