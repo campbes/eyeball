14e0f0b3a382ac6f9a7a2803251bcd22a248ac38
jamtrousers <jamtrousers@gmail.com>
2013-10-17 16:59:20 +0100

    hook some cool stuff up! totals, popovers (grade breakdown), filter-clickthroughs by build

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,7 @@
 var eyeballApp = angular.module('eyeballApp',[
     'ngRoute',
-    'eyeballControllers'
+    'eyeballControllers',
+    'ui.bootstrap'
 ]);
 
 eyeballApp.factory('socket', function ($rootScope) {
@@ -34,13 +35,30 @@ eyeballApp.factory('socket', function ($rootScope) {
     return connection;
 });
 
+eyeballApp.service('testDataStore',function(){
+
+    var testData = {};
+
+    return {
+        get : function() {
+            return testData;
+        },
+        set : function(data) {
+            testData = data;
+        }
+    }
+
+});
+
 eyeballApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
             when('/report', {
-                templateUrl: '/partials/report'
+                templateUrl: '/partials/report',
+                controller : 'ReportCtrl'
             }).when('/report/:query', {
-                templateUrl: '/partials/report'
+                templateUrl: '/partials/report',
+                controller : 'ReportCtrl'
             }).
             otherwise({
                   redirectTo: '/report'
@@ -49,11 +67,12 @@ eyeballApp.config(['$routeProvider',
 
 var eyeballControllers = angular.module('eyeballControllers',[]);
 
-eyeballControllers.controller('ReportCtrl',['$scope','$http','$routeParams','socket',
+eyeballControllers.controller('ReportCtrl',['$scope','$http','$routeParams','$timeout',
 
-    function ReportCtrl($scope,$http,$routeParams,socket) {
+    function ReportCtrl($scope,$http,$routeParams,$timeout) {
         console.log("ReportCtrl");
         $scope.results = [];
+        $scope.totals = {};
 
         var queryString = ($routeParams.query ? $routeParams.query.split(":")[1] : '');
         var query = queryString.split("&");
@@ -63,58 +82,84 @@ eyeballControllers.controller('ReportCtrl',['$scope','$http','$routeParams','soc
             if(qComp[1]) {
                 queryParams[qComp[0]] = qComp[1];
             }
-            console.log(queryParams);
         }
-
-        if(queryParams.build) {
-
-            $scope.$on("test_"+queryParams.build,function(){
-                console.log("Listened to test broadcast");
-
-                $scope.testInfo = {
-                    testing : true,
-                    progress : 0,
-                    status : "active",
-                    message : "Testing..."
-                };
-
-                var conn = socket();
-
-                conn.on("commitRecord_"+queryParams.build,function(data) {
-                    console.log("listened");
-                    $scope.testInfo.progress = data.progress;
-                    $scope.results.push(data.record);
-                    if(data.progress === 100) {
-                        console.log("disconnecting");
-                        conn.disconnect();
-                        $scope.testInfo.status = "";
-                        $scope.testInfo.message = "Testing...done!";
-                    }
-                });
-            });
-        }
+        $scope.query = queryParams;
 
         $http({
             url: '/report?'+queryString,
             method: "GET"
         }).success(function(results) {
             $scope.results = results;
+            $scope.updateTotals();
         });
+
+        $scope.updateTotals = function() {
+            $scope.totals = {
+                time : totals.getTotal($scope.results,'yslow','lt'),
+                yslow : {
+                    o : totals.getTotal($scope.results,'yslow','o')
+                },
+                dommonster : {
+                    COMPOSITE_stats : totals.getTotal($scope.results,'dommonster','COMPOSITE_stats')
+                }
+            };
+        };
+
+        $scope.popover = function(e,metric) {
+            $scope.popoverContent = metric;
+            $timeout(function(){
+                var el = $(e.srcElement);
+                el.popover({
+                    html : true,
+                    content : $('#popoverContent').html(),
+                    placement: 'left',
+                    container : 'body',
+                    trigger : 'hover'
+                }).popover('show');
+            },1);
+        }
 
     }
 ]);
 
-eyeballControllers.controller('TestCtrl',['$rootScope','$scope','$http','$location','$timeout',
+eyeballControllers.controller('TestCtrl',['$scope','$http','$location','testDataStore','socket',
 
-    function TestCtrl($rootScope,$scope,$http,$location,$timeout) {
-        $scope.testCriteria = {};
+    function TestCtrl($scope,$http,$location,testDataStore,socket) {
         console.log("TestCtrl");
+        $scope.testCriteria = {};
+        var testData = testDataStore.get()
+        $scope.testInfo = testData;
+
+        if(testData.testing === true) {
+            var conn = socket();
+            conn.on("commitRecord_"+testData.build,function(data) {
+                console.log("listened");
+                console.log($scope.results);
+                $scope.testInfo.progress = data.progress;
+                $scope.results.push(data.record);
+                $scope.updateTotals();
+                if(data.progress === 100) {
+                    console.log("disconnecting");
+                    conn.disconnect();
+                    $scope.testInfo.status = "";
+                    $scope.testInfo.message = "Testing...done!";
+                }
+            });
+        }
+
+
         $scope.test = function() {
             $scope.testCriteria.build =(new Date()).getTime().toString() + Math.random().toString();
+
+            testDataStore.set({
+                testing : true,
+                progress : 0,
+                status : "active",
+                message : "Testing...",
+                build : $scope.testCriteria.build
+            });
+
             $location.path('/report/:build='+$scope.testCriteria.build);
-            $timeout(function(){
-                $rootScope.$broadcast("test_"+$scope.testCriteria.build);
-            },1);
 
             $http({
                 url: '/test',
@@ -128,3 +173,84 @@ eyeballControllers.controller('TestCtrl',['$rootScope','$scope','$http','$locati
     }
 
 ]);
+
+
+
+
+var totals = (function() {
+
+    function getPC(results,tool,measure) {
+        var fails = 0;
+        var count = 0;
+        var i = null;
+        var res = null;
+        var grade = null;
+
+        for (i=results.length-1; i>=0; i--) {
+            if(!results[i].metrics) {
+                continue;
+            }
+            res = results[i].metrics[tool];
+            if(!res || !res.grades) {
+                continue;
+            }
+
+            grade = accessObject(res.grades,measure);
+
+            if(!grade) {
+                continue;
+            }
+            count += 1;
+            if(grade !== "A" && grade !== "B") {
+                fails +=1;
+            }
+        }
+        return Math.floor(100 - (fails/(count/100)));
+    }
+
+    function getTotal(results,tool,measure) {
+
+        var pc = getPC(results,tool,measure);
+        var total = {
+            score : pc,
+            grade : "",
+            message : "",
+            class : ""
+        };
+
+        if(pc > 85) {
+            total.grade = "A";
+            total.message = "PASS";
+            total.class = "success";
+        } else if (pc <= 85) {
+            total.grade = "F";
+            total.message = "FAIL";
+            total.class = "danger";
+        }
+        return total;
+    }
+
+    return {
+        getTotal : getTotal
+    };
+
+}());
+
+function accessObject(obj,str) {
+
+    function getProp(obj,key) {
+        return obj[key];
+    }
+
+    var keys = str.split(".");
+    var keysLength = keys.length;
+    var i = null;
+
+    for (i=0; i<keysLength; i++) {
+        if(!obj) {
+            return null;
+        }
+        obj = getProp(obj,keys[i]);
+    }
+    return obj;
+}
--- routes/report.js
+++ routes/report.js
@@ -1,13 +1,73 @@
 module.exports = function(req,res) {
 
     var url = url = require('url');
-    var query = url.parse(req.url, true).query || {};
 
-    var dbQuery = {};
+    function getDbQuery() {
+        var queryString = url.parse(req.url, true).query || {};
+        dbQuery = {};
 
-    if(query.build) {
-        dbQuery.build = query.build.toString();
+        var id = queryString.id;
+        if(id) {
+            dbQuery._id = mongojs.ObjectId(id);
+            return dbQuery;
+        }
+
+        function getMultiValues(key) {
+            var val = queryString[key];
+            if(val) {
+                if(typeof val === "string" && val.indexOf(",") !== -1) {
+                    val = val.split(",");
+                }
+                if(typeof val  === "string") {
+                    return val;
+                } else {
+                    return {
+                        $in : val
+                    }
+                }
+
+            }
+            return null;
+        }
+
+        var build = getMultiValues("build");
+        var tag = getMultiValues("tag");
+        var start = (queryString.start ? new Date(queryString.start) : "");
+        var end = (queryString.end ? new Date(queryString.end) : "");
+
+        if(build) {
+            dbQuery.build = build;
+        }
+        if(tag) {
+            dbQuery.tag = tag;
+        }
+
+        if(start && end) {
+            dbQuery.timestamp = {
+                $gte : start, $lte : end
+            };
+        } else if (start) {
+            dbQuery.timestamp = {
+                $gte : start
+            };
+        } else if (end) {
+            dbQuery.timestamp = {
+                $lte : end
+            };
+        }
+
+        if(queryString.url) {
+            dbQuery.url = queryString.url
+        } else if(queryString.urlRegex) {
+            dbQuery.url = {
+                $regex : queryString.urlRegex
+            };
+        }
+
+        return dbQuery;
     }
+
+    var dbQuery = getDbQuery();
 
     DB.find(dbQuery,function(err,results) {
         if(err) {
