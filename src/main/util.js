0390a7be6c40311cd9c98970f09e1fdc388d99df
jamtrousers <jamtrousers@gmail.com>
2014-03-27 16:39:04 +0000

    move dbQuery into a util class. Allow multiple ids in queries - will enable comparison of individual records

---
+++ src/main/util.js
@@ -0,0 +1,83 @@
+var util = function() {
+
+    function getDbQuery(req) {
+        var url = require('url');
+        var mongojs = require('mongojs');
+        var queryString = url.parse(req.url, true).query || {};
+        var dbQuery = {};
+
+        function getMultiValues(key) {
+            var val = queryString[key];
+            if(val) {
+                if(typeof val === "string" && val.indexOf(",") !== -1) {
+                    val = val.split(",");
+                }
+                if(typeof val  === "string") {
+                    return val;
+                }
+                return {
+                    $in : val
+                };
+            }
+            return null;
+        }
+
+        var build = getMultiValues("build");
+        var tag = getMultiValues("tag");
+        var start = (queryString.start ? new Date(queryString.start) : "");
+        var end = (queryString.end ? new Date(new Date(queryString.end).getTime() + 86399999) : "");
+
+        if(build) {
+            dbQuery.build = build;
+        }
+        if(tag) {
+            dbQuery.tag = tag;
+        }
+
+        if(start && end) {
+            dbQuery.timestamp = {
+                $gte : start, $lte : end
+            };
+        } else if (start) {
+            dbQuery.timestamp = {
+                $gte : start
+            };
+        } else if (end) {
+            dbQuery.timestamp = {
+                $lte : end
+            };
+        }
+
+        if(queryString.urlExact && queryString.url) {
+            dbQuery.url = queryString.url;
+        } else if (queryString.url) {
+            dbQuery.url = {
+                $regex : queryString.url
+            };
+        }
+
+        if(queryString.id) {
+            var id = queryString.id.split(",");
+            var i;
+            for(i=id.length-1; i>=0;i--) {
+                id[i] = mongojs.ObjectId(id[i]);
+            }
+            if(id.length > 1) {
+                dbQuery._id = {
+                    $in : id
+                };
+            } else if (id[0]) {
+                dbQuery._id = id[0];
+            }
+        }
+
+        return dbQuery;
+    }
+
+    return {
+        getDbQuery : getDbQuery
+    }
+
+};
+
+module.exports = util();
---
+++ src/test/utilSpec.js
@@ -0,0 +1,19 @@
+describe("tests the util methods",function() {
+
+     it("tests that getDbQuery returns the proper query object",function(){
+         require('url').init({
+             query : {
+                 build : "jeff,smith",
+                 tag : "badgers",
+                 start : "010101",
+                 url : "test.com"
+             }
+         });
+         var query = util().getDbQuery({});
+         expect(query.build.$in[1]).toBe("smith");
+         expect(query.tag).toBe("badgers");
+         expect(query.timestamp.$gte.getTime()).toBe(256589596800000);
+         expect(query.url.$regex).toBe("test.com");
+     });
+
+});
