649989b79c3766333e3ba64994646c401f4ab48f
jamtrousers <jamtrousers@gmail.com>
2014-03-31 11:36:23 +0100

    unit test coverage

--- src/test/controllers/test/pageSpec.js
+++ src/test/controllers/test/pageSpec.js
@@ -1,14 +1,45 @@
 describe("tests the test controller page module",function() {
 
-    var controller;
+    var controller, page;
 
     beforeEach(function() {
         require = helpers.require;
         controller = EyeballControllersTestPage();
+        page = controller.setup({
+            setFn : function(name,cb){
+                cb();
+            },
+            evaluate : function() {}
+        });
     });
 
-    it("checks that the test controller initialises correctly",function() {
-        expect(controller).toBeDefined();
+    it("tests onResourceRequested sets the correct properties",function() {
+        page.onResourceRequested([{
+            id : "test"
+        }]);
+        expect(page.resources["test"].request.id).toBe("test");
+    });
+
+    it("tests onResourceReceived adds the startReply",function() {
+        page.onResourceRequested([{
+            id : "test"
+        }]);
+        page.onResourceReceived({
+            id : "test",
+            stage : "start"
+        });
+        expect(page.resources["test"].startReply.id).toBe("test");
+    });
+
+    it("tests onResourceReceived adds the endReply",function() {
+        page.onResourceRequested([{
+            id : "test"
+        }]);
+        page.onResourceReceived({
+            id : "test",
+            stage : "end"
+        });
+        expect(page.resources["test"].endReply.id).toBe("test");
     });
 
 });
