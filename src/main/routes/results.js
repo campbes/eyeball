16236f9688bacc0142720e3b392dc046639ba6dc
jamtrousers <jamtrousers@gmail.com>
2014-06-15 22:01:48 +0100

    add proper caching of results, using latest timestamp in resultset as last-modified

--- src/main/routes/results.js
+++ src/main/routes/results.js
@@ -1,6 +1,7 @@
 var EyeballRoutesResults = (function() {
 
 var _ = require('lodash');
+var resultsType = "results";
 
 function results(req,res) {
     var url = require('url');
@@ -32,56 +33,30 @@ function results(req,res) {
     }
 
     eyeball.DB.find(dbQuery,cfg,{
-        limit : 1000
+        limit : (resultsType === "latest" ? 10000 : 1000)
     }).sort({timestamp : -1},
         function(err,results) {
             if(err) {
                 res.send(err);
             }
-            res.send(JSON.stringify(results));
+            if(resultsType === "latest") {
+                results = _.unique(results,"url");
+            }
+            res.setHeader("Content-type","application/json");
+            res.setHeader("Cache-control","max-age=3600,must-revalidate");
+            res.setHeader("Last-modified",results[0].timestamp);
+            if(!req.get("if-modified-since") || new Date(results[0].timestamp).setMilliseconds(0)  > new Date(req.get("if-modified-since"))) {
+                res.send(JSON.stringify(results));
+            } else {
+                res.send(304);
+            }
         });
 
-    };
+    }
 
     function latest(req,res) {
-        var url = require('url');
-            var queryString = url.parse(req.url, true).query || {};
-            var fields = queryString.fields;
-            var i = 0;
-
-            var dbQuery = require('../util').getDbQuery(req);
-            var cfg = {
-                url : 1,
-                timestamp: 1,
-                build : 1,
-                tag : 1
-            };
-
-            if(fields) {
-                fields = fields.split(",");
-                for(i=fields.length-1; i>=0; i--) {
-                    cfg[fields[i]] = 1;
-                }
-            } else {
-                var rep;
-                var reportCfg = require('../conf/report');
-                for(i=reportCfg.reports.length-1; i>=0; i--) {
-                    rep = reportCfg.reports[i];
-                    cfg["metrics."+rep+".tool"] = 1;
-                    cfg["metrics."+rep+".grades"] = 1;
-                }
-            }
-
-            eyeball.DB.find(dbQuery,cfg,{
-                limit : 10000
-            }).sort({timestamp : -1},
-                function(err,results) {
-                    if(err) {
-                        res.send(err);
-                    }
-                    results = _.unique(results,"url");
-                    res.send(JSON.stringify(results));
-                });
+        resultsType = "latest";
+        results(req,res);
     }
 
     return {
