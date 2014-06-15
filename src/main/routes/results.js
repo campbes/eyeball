fac4f8542c8e1d283446184e4ac9a1268919afaa
jamtrousers <jamtrousers@gmail.com>
2014-06-15 22:10:27 +0100

    add proper caching of results, using latest timestamp in resultset as last-modified

--- src/main/routes/results.js
+++ src/main/routes/results.js
@@ -1,7 +1,6 @@
 var EyeballRoutesResults = (function() {
 
 var _ = require('lodash');
-var resultsType = "results";
 
 function results(req,res,resultsType) {
     resultsType = resultsType || "results";
