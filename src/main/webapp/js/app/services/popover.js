82b5183966dd4ff61a6e771034a8704d1a3fa5d9
campbes <stuartjcampbell@gmail.com>
2014-10-19 17:54:43 +0100

    general ui improvements

--- src/main/webapp/js/app/services/popover.js
+++ src/main/webapp/js/app/services/popover.js
@@ -4,14 +4,14 @@ eyeballApp.factory('popover',function(){
     return {
         show : function(e,obj) {
             var el = $(obj);
-            el.popup('removeAll');
+            el.popup({on : false});
             el.popup('setting','content',$('#popoverContent').html());
             el.popup('setting','variation','small');
             el.popup('setting','position','left center');
             el.popup('show');
         },
         hide : function(e,obj) {
-
+            $(obj).popup('hide');
         }
     };
 });
