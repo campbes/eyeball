254fc4a546171ad9b8eb8c325a47c04a6bbf22d9
Stuart Campbell <stuart.campbell@trinitymirror.com>
2015-03-02 17:08:40 +0000

    remove popup hide animation - tricky to stop it going wrong!

--- src/main/webapp/js/app/services/popover.js
+++ src/main/webapp/js/app/services/popover.js
@@ -1,24 +1,29 @@
 /*global eyeballApp, $*/
 
 eyeballApp.factory('popover',function(){
-    return {
-        show : function(e,obj) {
-            var el = $(obj);
-            el.popup({on : false});
-            el.popup('hide');
-            el.popup('setting','content',$('#popoverContent').html());
-            el.popup('setting','variation','small');
-            el.popup('setting','position','left center');
-            el.data('delay',setTimeout(function() {
-                  el.popup('show');
-            },500));
-        },
-        hide : function(e,obj) {
-            var el = $(obj);
-            if(el.data('delay')) {
-                clearTimeout(el.data('delay'));
-            }
-            el.popup('hide');
+
+    function show(e,obj) {
+        var el = $(obj);
+        el.popup({on : false});
+        hide(e,obj);
+        el.popup('setting','content',$('#popoverContent').html());
+        el.popup('setting','variation','small');
+        el.popup('setting','position','left center');
+        el.data('delay',setTimeout(function() {
+            el.popup('show');
+        },500));
+    }
+
+    function hide(e,obj) {
+        var el = $(obj);
+        if(el.data('delay')) {
+            clearTimeout(el.data('delay'));
         }
+        el.popup('remove');
+    }
+
+    return {
+        show : show,
+        hide : hide
     };
 });
